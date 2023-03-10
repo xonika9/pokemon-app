ff9c41efa10a57f9a6ad987d5e46d5519cea47ff
x9 <xonika9@gmail.com>
2023-03-10 14:27:11 +0300

    get and show all pokemons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [pokemonData, setPokemonData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=150')
+      .then((response) => response.json())
+      .then((data) => {
+        setPokemonData(data.results);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="App-title">Pokémon List</h1>
       </header>
+      <div className="pokemon-container">
+        {pokemonData.map((pokemon, index) => (
+          <div key={index} className="pokemon-card">
+            <div className="pokemon-image">
+              <img
+                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+                  index + 1
+                }.png`}
+                alt={`${pokemon.name}`}
+              />
+            </div>
+            <div className="pokemon-details">
+              <span className="pokemon-name">{pokemon.name}</span>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
