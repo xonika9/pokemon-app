ef7fb0f0c1e382d982d6ae13cfbb58762f3d3f74
x9 <xonika9@gmail.com>
2023-03-10 15:25:19 +0300

    add axios & loading message

--- src/App.js
+++ src/App.js
@@ -1,83 +1,62 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
   const [pokemonData, setPokemonData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=1500')
-      .then((response) => response.json())
-      .then((data) => {
-        setPokemonData(data.results);
-      });
-  }, []);
-
-  const getPokemonDetails = async (pokemon) => {
-    const response = await fetch(pokemon.url);
-    const data = await response.json();
-    return data;
-  };
-
-  const formatPokemonDetails = (details) => {
-    return {
-      hp: details.stats[0].base_stat,
-      attack: details.stats[1].base_stat,
-      defense: details.stats[2].base_stat,
-      specialAttack: details.stats[3].base_stat,
-      specialDefense: details.stats[4].base_stat,
-      speed: details.stats[5].base_stat,
-    };
-  };
-
-  const [pokemonDetails, setPokemonDetails] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const details = await Promise.all(
-        pokemonData.map(async (pokemon) => {
-          const details = await getPokemonDetails(pokemon);
-          return formatPokemonDetails(details);
-        }),
-      );
-      setPokemonDetails(details);
-    };
+    async function fetchData() {
+      try {
+        const response = await axios.get(
+          'https://pokeapi.co/api/v2/pokemon?limit=1500',
+        );
+        const data = response.data.results;
+        const pokemonData = await Promise.all(
+          data.map(async (pokemon) => {
+            const pokemonResponse = await axios.get(pokemon.url);
+            return pokemonResponse.data;
+          }),
+        );
+        setPokemonData(pokemonData);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    }
     fetchData();
-  }, [pokemonData]);
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="App-title">Pokémon List</h1>
+        <h1 className="App-title">Pokémon App</h1>
       </header>
-      <div className="pokemon-container">
-        {pokemonData.map((pokemon, index) => (
-          <div key={index} className="pokemon-card">
-            <div className="pokemon-image">
-              <img
-                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-                  index + 1
-                }.png`}
-                alt={`${pokemon.name}`}
-              />
-            </div>
-            <div className="pokemon-details">
-              <span className="pokemon-name">{pokemon.name}</span>
-              <div className="pokemon-stats">
-                <div>HP: {pokemonDetails[index]?.hp}</div>
-                <div>Attack: {pokemonDetails[index]?.attack}</div>
-                <div>Defense: {pokemonDetails[index]?.defense}</div>
-                <div>
-                  Special Attack: {pokemonDetails[index]?.specialAttack}
+      {loading ? (
+        <p className="loading">Loading...</p>
+      ) : (
+        <div className="pokemon-container">
+          {pokemonData.map((pokemon) => (
+            <div className="pokemon-card" key={pokemon.id}>
+              <div className="pokemon-image">
+                <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+              </div>
+              <div className="pokemon-details">
+                <h2 className="pokemon-name">{pokemon.name}</h2>
+                <div className="pokemon-stats">
+                  <div>HP: {pokemon.stats[0].base_stat}</div>
+                  <div>Attack: {pokemon.stats[1].base_stat}</div>
+                  <div>Defense: {pokemon.stats[2].base_stat}</div>
+                  <div>Special Attack: {pokemon.stats[3].base_stat}</div>
+                  <div>Special Defense: {pokemon.stats[4].base_stat}</div>
+                  <div>Speed: {pokemon.stats[5].base_stat}</div>
                 </div>
-                <div>
-                  Special Defense: {pokemonDetails[index]?.specialDefense}
-                </div>
-                <div>Speed: {pokemonDetails[index]?.speed}</div>
               </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
