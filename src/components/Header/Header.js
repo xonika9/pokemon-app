9b573f4523ab532879f29623ddb569a8257ad8df
x9 <xonika9@gmail.com>
2023-03-17 15:23:06 +0300

    add search

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,18 @@
 import './Header.css';
 
 function Header(props) {
-  const { sortOrder, handleSortChange } = props;
+  const { sortOrder, handleSortChange, handleSearchChange } = props;
 
   return (
     <header className="app-header">
       <a className="logo header__logo" href="/"></a>
       <h1 className="app-title">x9Pokémon</h1>
+      <input
+        className="search-container"
+        type="text"
+        placeholder="Search"
+        onChange={handleSearchChange}
+      />
       <div className="select-container">
         <label htmlFor="sort-by-select">Sort by:</label>
         <select value={sortOrder} onChange={handleSortChange}>
