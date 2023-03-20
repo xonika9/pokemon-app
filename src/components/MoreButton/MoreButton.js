0e0bafcc6cb0c5abfe1f42b5c265064388819510
x9 <xonika9@gmail.com>
2023-03-20 21:25:20 +0300

    refactor for more button

--- src/components/MoreButton/MoreButton.js
+++ src/components/MoreButton/MoreButton.js
@@ -4,8 +4,11 @@ function MoreButton({ numCards, filteredPokemon, addCards }) {
   return (
     <>
       {numCards < filteredPokemon.length && (
-        <div className="more-container">
-          <button className="more-button" onClick={addCards}>
+        <div className="flex justify-center py-5 px-0">
+          <button
+            className="more-button cursor-pointer rounded border-none bg-green-500 py-2 px-36 text-base text-white hover:bg-green-600"
+            onClick={addCards}
+          >
             More
           </button>
         </div>
