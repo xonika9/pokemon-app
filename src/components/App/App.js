67d3d56ce85788f57832c285476b88750c3ade4d
x9 <xonika9@gmail.com>
2023-03-20 10:42:43 +0300

    fix

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useMemo } from 'react';
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import axios from 'axios';
 import './App.css';
