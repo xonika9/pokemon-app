85db8982f41afe266e86711f66d9ac965c274f40
x9 <xonika9@gmail.com>
2023-03-23 15:42:36 +0300

    add bg images to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'clear-bin': "url('./images/remove-bin.svg')",
+        'minimize-button': "url('./images/minimize.svg')",
+        'maximize-button': "url('./images/maximize.svg')",
+        'pokemon-logo': "url('./images/poke-logo.png')",
+        'like-transp': "url('./images/like-transp.svg')",
+        'like-red': "url('./images/like-red.svg')",
+        'clear-button': "url('./images/clear-button.svg')",
+        'compare-unchecked': "url('./images/unchecked-icon.svg')",
+        'compare-checked': "url('./images/checked-icon.svg')",
+      },
+    },
   },
   plugins: [],
 };
