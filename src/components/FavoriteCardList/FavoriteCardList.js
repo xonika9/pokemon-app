518dad61701731fa88c356fd41a1a01c9a857036
x9 <xonika9@gmail.com>
2023-03-23 15:44:08 +0300

    refactor fav card list

--- src/components/FavoriteCardList/FavoriteCardList.js
+++ src/components/FavoriteCardList/FavoriteCardList.js
@@ -12,43 +12,43 @@ function FavoriteCardList({
   return (
     <>
       {minimized && (
-        <div className="fav-list-minimized fixed bottom-16 right-0 shadow-md">
+        <div className="fixed bottom-16 right-0 p-2 pr-8 shadow-none">
           {favorites.length > 0 && (
             <button
               onClick={() => setFavorites([])}
-              className="clear-fav-button-min absolute top-0 right-0 h-7 w-7 bg-contain bg-center bg-no-repeat"
+              className="bg-clear-bin absolute top-0 right-0 h-7 w-7 cursor-pointer bg-contain bg-center bg-no-repeat"
             ></button>
           )}
           <button
             onClick={() => setMinimized(false)}
-            className="fav-max-button fixed bottom-16 right-0 h-7 w-7 bg-contain bg-center bg-no-repeat"
+            className="bg-maximize-button fixed bottom-16 right-0 h-7 w-7 bg-contain bg-center bg-no-repeat"
           ></button>
-          <div className="fav-container-min flex flex-col-reverse items-start">
+          <ul className="flex max-h-[70vh] flex-col flex-wrap-reverse content-start items-start gap-2 p-0">
             <FavoriteCardMin
               favorites={favorites}
               handleRemoveFavorite={handleRemoveFavorite}
             />
-          </div>
+          </ul>
         </div>
       )}
       {!minimized && (
-        <div className="fav-list-maximized fixed bottom-16 right-0 max-w-md rounded-md border bg-white p-4 shadow-md">
+        <div className="fixed bottom-16 right-0 max-w-[322px] rounded-md border bg-white p-4 shadow-md md:max-w-[464px]">
           {favorites.length > 0 && (
             <button
               onClick={() => setFavorites([])}
-              className="clear-fav-button absolute top-0 right-0 h-7 w-7 bg-contain bg-center bg-no-repeat"
+              className="bg-clear-bin bg-centerbg-no-repeat absolute top-0 right-0 h-7 w-7 cursor-pointer bg-contain"
             ></button>
           )}
           <button
             onClick={() => setMinimized(true)}
-            className="fav-min-button fixed bottom-16 right-0 h-7 w-7 bg-contain bg-center bg-no-repeat"
+            className="bg-minimize-button fixed bottom-16 right-0 h-7 w-7 bg-contain bg-center bg-no-repeat"
           ></button>
-          <div className="fav-container flex flex-wrap justify-center gap-3">
+          <ul className="flex flex-wrap justify-center gap-3 p-0 pr-4">
             <FavoriteCard
               favorites={favorites}
               handleRemoveFavorite={handleRemoveFavorite}
             />
-          </div>
+          </ul>
         </div>
       )}
     </>
