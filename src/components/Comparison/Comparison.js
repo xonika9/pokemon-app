d8a052d66ded80a751cfbe6688715ff7d8371319
x9 <xonika9@gmail.com>
2023-03-23 15:43:31 +0300

    refactor comparison

--- src/components/Comparison/Comparison.js
+++ src/components/Comparison/Comparison.js
@@ -22,7 +22,7 @@ const Comparison = ({
         {comparisonList.length > 0 && (
           <button
             onClick={() => setComparisonList([])}
-            className="comp-clear h-7 w-7 cursor-pointer border-none bg-contain bg-no-repeat text-center"
+            className="bg-clear-bin h-7 w-7 cursor-pointer border-none bg-contain bg-no-repeat text-center"
           ></button>
         )}
       </div>
@@ -40,7 +40,7 @@ const Comparison = ({
                   className="sticky border border-solid border-gray-500 bg-blue-100 p-2 text-center capitalize"
                 >
                   <button
-                    className="card-compare-checked absolute top-0 right-0 m-3 mx-2 h-5 w-5 cursor-pointer border-none bg-none bg-contain bg-no-repeat p-0 text-center transition-all duration-300"
+                    className="bg-compare-checked absolute top-0 right-0 m-3 mx-2 h-5 w-5 cursor-pointer border-none bg-contain bg-no-repeat p-0 text-center transition-all duration-300"
                     onClick={() => handleToggleCompare(pokemon)}
                   ></button>
                   <img
