f0ec3728a944eb4475d5c1f8f124833e6db664e4
x9 <xonika9@gmail.com>
2023-03-23 15:45:34 +0300

    refactor card

--- src/components/PokemonCard/PokemonCard.js
+++ src/components/PokemonCard/PokemonCard.js
@@ -12,73 +12,57 @@ function PokemonCard({
   return (
     <>
       {visiblePokemon.map((pokemon, index) => (
-        <div
-          className="pokemon-card flex flex-col justify-between rounded bg-white p-4 shadow-md"
+        <li
+          className="flex flex-col justify-between rounded-xl bg-white p-2 shadow-md md:p-4"
           key={index}
         >
-          <div className="flex justify-center overflow-hidden">
+          <div className="flex justify-center overflow-hidden rounded-lg">
             <LazyLoad height={200}>
               <img
                 src={pokemon.image}
                 alt={pokemon.name}
-                className="pokemon-image h-52 max-w-full object-contain"
+                className="h-36 max-w-full object-contain md:h-52"
               />
             </LazyLoad>
           </div>
           <div className="mt-4 text-center">
-            <div className="flex items-center justify-center">
-              {!favorites.some(
-                (favorite) => favorite.name === pokemon.name,
-              ) && (
+            <div className="flex items-baseline justify-center gap-2">
+              {favorites.some((favorite) => favorite.name === pokemon.name) ? (
                 <button
-                  className="card-like-save h-8 w-8 cursor-pointer border-none bg-none bg-no-repeat p-0 text-center transition-all duration-300"
+                  className="bg-like-red hover:bg-clear-button h-4 w-4 cursor-pointer border-none bg-transparent bg-contain bg-no-repeat p-0 text-center transition-all duration-300"
+                  onClick={() => handleRemoveFavorite(pokemon)}
+                ></button>
+              ) : (
+                <button
+                  className="bg-like-transp h-4 w-4 cursor-pointer border-none bg-contain bg-no-repeat p-0 text-center transition-all duration-300"
                   onClick={() => handleAddFavorite(pokemon)}
                 ></button>
               )}
-              {favorites.some((favorite) => favorite.name === pokemon.name) && (
-                <button
-                  className="card-like-active h-8 w-8 cursor-pointer border-none bg-transparent bg-none bg-no-repeat p-0 text-center transition-all duration-300"
-                  onClick={() => handleRemoveFavorite(pokemon)}
-                ></button>
-              )}
-              <h2 className="m-0 text-2xl font-bold capitalize text-gray-900">
+              <h2 className="m-0 text-lg font-bold capitalize text-gray-900 md:text-2xl">
                 {pokemon.name}
               </h2>
-              {!comparisonList.some((p) => p.name === pokemon.name) && (
+              {comparisonList.some((p) => p.name === pokemon.name) ? (
                 <button
-                  className="card-compare-unchecked mx-2 h-5 w-5 cursor-pointer border-none bg-none bg-contain bg-no-repeat p-0 text-center transition-all duration-300"
+                  className="bg-compare-checked h-5 w-5 cursor-pointer border-none bg-contain bg-no-repeat p-0 text-center transition-all duration-300"
                   onClick={() => handleToggleCompare(pokemon)}
                 ></button>
-              )}
-              {comparisonList.some((p) => p.name === pokemon.name) && (
+              ) : (
                 <button
-                  className="card-compare-checked mx-2 h-5 w-5 cursor-pointer border-none bg-none bg-contain bg-no-repeat p-0 text-center transition-all duration-300"
+                  className="bg-compare-unchecked h-5 w-5 cursor-pointer border-none bg-contain bg-no-repeat p-0 text-center transition-all duration-300"
                   onClick={() => handleToggleCompare(pokemon)}
                 ></button>
               )}
             </div>
-            <div className="mt-4 flex flex-col flex-wrap justify-center">
-              <p className="pokemon-stat text-lg">
-                HP: {pokemon.stats[0].base_stat}
-              </p>
-              <p className="pokemon-stat text-lg">
-                Attack: {pokemon.stats[1].base_stat}
-              </p>
-              <p className="pokemon-stat text-lg">
-                Defense: {pokemon.stats[2].base_stat}
-              </p>
-              <p className="pokemon-stat text-lg">
-                Special Attack: {pokemon.stats[3].base_stat}
-              </p>
-              <p className="pokemon-stat text-lg">
-                Special Defense: {pokemon.stats[4].base_stat}
-              </p>
-              <p className="pokemon-stat text-lg">
-                Speed: {pokemon.stats[5].base_stat}
-              </p>
-            </div>
+            <ul className="mt-4 grid grid-cols-2 gap-x-2 gap-y-1">
+              <li className="text-lg">HP: {pokemon.stats[0].base_stat}</li>
+              <li className="text-lg">Attack: {pokemon.stats[1].base_stat}</li>
+              <li className="text-lg">Defense: {pokemon.stats[2].base_stat}</li>
+              <li className="text-lg">Sp. Atk: {pokemon.stats[3].base_stat}</li>
+              <li className="text-lg">Sp. Def: {pokemon.stats[4].base_stat}</li>
+              <li className="text-lg">Speed: {pokemon.stats[5].base_stat}</li>
+            </ul>
           </div>
-        </div>
+        </li>
       ))}
     </>
   );
